9f15c891145f6f7366dc1ab67bfbdd3ff1e12be6
leonelmtz9914 <42593226+leonelmtz9914@users.noreply.github.com>
2021-05-11 23:18:08 -0500

    version1

---
+++ app/src/main/java/com/example/project_startup_cheffy/adapter/IngredientItem.java
@@ -0,0 +1,31 @@
+package com.example.project_startup_cheffy.adapter;
+
+
+
+
+public class IngredientItem {
+
+    private String ingredient;
+    private String quantity;
+
+    public IngredientItem(String ingredient, String quantity) {
+        setIngredient(ingredient);
+        setQuantity(quantity);
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+}
